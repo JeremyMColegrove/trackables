619d7846988fd68e6b083e714ab0bcc5fce62e3e
Jeremy Colegrove <jeremy@Mac.lan>
2026-03-21 10:46:23 -0500

    pino pretty external dep

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { withGTConfig } from "gt-next/config";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "standalone",
+	serverExternalPackages: ["pino", "pino-pretty"],
 };
 
 export default withGTConfig(nextConfig);
